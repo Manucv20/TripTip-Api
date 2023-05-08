92bac3961076a6d3b0f05ce96e337d88189d7b59
Manucv20 <manusevilla20@gmail.com>
2023-05-08 19:45:08 +0200

    Init BBDD

--- ProyectoTrip-init.sql
+++ ProyectoTrip-init.sql
@@ -1,45 +1,48 @@
+use ProyectoTrip;
+
 CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  password CHAR(60) NOT NULL,
-  profile_photo VARCHAR(255),
-  PRIMARY KEY (id),
-  UNIQUE KEY (email)
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50) NOT NULL,
+lastname varchar(50),
+address text not null,
+gender varchar(10) not null,
+email VARCHAR(90) NOT NULL UNIQUE,
+password VARCHAR(90) NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+profile_image VARCHAR(255),
+bio varchar(255)
 );
 
 CREATE TABLE recommendations (
-  id INT NOT NULL AUTO_INCREMENT,
-  user_id INT NOT NULL,
-  title VARCHAR(100) NOT NULL,
-  category VARCHAR(50) NOT NULL,
-  location VARCHAR(100) NOT NULL,
-  summary VARCHAR(255) NOT NULL,
-  description TEXT NOT NULL,
-  photo VARCHAR(255),
-  votes BIGINT NOT NULL DEFAULT 0,
-  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
-);
-
-CREATE TABLE comments (
-  id INT NOT NULL AUTO_INCREMENT,
-  recommendation_id INT NOT NULL,
-  user_id INT NOT NULL,
-  content TEXT NOT NULL,
-  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id),
-  FOREIGN KEY (recommendation_id) REFERENCES recommendations(id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
+id INT PRIMARY KEY AUTO_INCREMENT,
+user_id INT NOT NULL,
+title VARCHAR(100) NOT NULL,
+category VARCHAR(50) NOT NULL,
+location VARCHAR(100) NOT NULL,
+summary VARCHAR(100) NOT NULL,
+details TEXT NOT NULL,
+image VARCHAR(255),
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE votes (
-  id INT NOT NULL AUTO_INCREMENT,
-  user_id INT,
-  recommendation_id INT NOT NULL,
-  vote_value TINYINT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (recommendation_id) REFERENCES recommendations(id)
+id INT PRIMARY KEY AUTO_INCREMENT,
+user_id INT NOT NULL,
+recommendation_id INT NOT NULL,
+value INT NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+FOREIGN KEY (recommendation_id) REFERENCES recommendations(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE comments (
+id INT PRIMARY KEY AUTO_INCREMENT,
+user_id INT NOT NULL,
+recommendation_id INT NOT NULL,
+comment TEXT NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+FOREIGN KEY (recommendation_id) REFERENCES recommendations(id) ON DELETE CASCADE
 );
